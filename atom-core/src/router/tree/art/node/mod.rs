1471775ba8784c06e872179764e85f05b36d003e
liyu <liyu@espressif.com>
2023-06-20 14:37:16 +0800

    完成Node实现

--- atom-core/src/router/tree/art/node/mod.rs
+++ atom-core/src/router/tree/art/node/mod.rs
@@ -50,9 +50,30 @@ impl NodeType {
 }
 
 #[derive(Clone, Copy)]
-pub struct NodePtr {
-    pub node_id: usize,
+pub union  NodePtr {
+    pub tid: usize,
     pub sub_node: *const BaseNode,
+}
+impl NodePtr {
+    #[inline]
+    pub(crate) fn from_node(ptr: *const BaseNode) -> Self {
+        Self { sub_node: ptr }
+    }
+
+    #[inline]
+    pub(crate) fn from_tid(tid: usize) -> Self {
+        Self { tid }
+    }
+
+    #[inline]
+    pub(crate) fn as_tid(&self) -> usize {
+        unsafe { self.tid }
+    }
+
+    #[inline]
+    pub(crate) fn as_ptr(&self) -> *const BaseNode {
+        unsafe { self.sub_node }
+    }
 }
 
 pub struct BaseNode {
--- atom-core/src/router/tree/art/node/node.rs
+++ atom-core/src/router/tree/art/node/node.rs
@@ -11,6 +11,12 @@ pub(crate) const NODE48TYPE: Result<Layout, LayoutError> = Layout::from_size_ali
 pub(crate) const NODE256TYPE: Result<Layout, LayoutError> = Layout::from_size_align(mem::size_of::<Node256>(),
                                                                                     mem::align_of::<Node256>(), );
 
+#[cfg(all(target_feature = "sse2", not(miri)))]
+use core::arch::x86_64::{_mm_prefetch, _MM_HINT_T0};
+use std::arch::x86_64::{
+    __m128i, _mm_cmplt_epi8, _mm_loadu_si128, _mm_movemask_epi8, _mm_set1_epi8,
+};
+
 #[repr(C)]
 #[repr(align(64))] // 常见的缓存行大小为64字节
 pub(crate) struct Node4 {
@@ -153,6 +159,207 @@ pub(crate) struct Node16 {
     children: [NodePtr; 16],
     keys: [u8; 16],
 }
+impl Node16 {
+
+    /// 翻转 u8 的符号位
+    fn flip_sign(val: u8) -> u8 {
+        val ^ 128
+    }
+
+    #[cfg(all(target_feature = "sse2", not(miri)))]
+    fn ctz(val: u16) -> u16 {
+        val.trailing_zeros() as u16
+    }
+
+    fn get_insert_pos(&self, key: u8) -> usize {
+        let flipped = Self::flip_sign(key);
+
+        #[cfg(all(target_feature = "sse2", not(miri)))]
+        {
+            unsafe {
+
+                // __m128i 是一个宽度为 128 位的整数向量类型。它可以存储和操作 16 个 8 位整数值。
+
+                // _mm_cmplt_epi8: 这是一个 SSE 指令，用于比较两个 __m128i 向量中对应元素的大小。
+                // 它返回一个 __m128i 向量，每个元素的值为 0 或 1，表示对应位置的元素是否满足小于的条件。
+                let cmp = _mm_cmplt_epi8(
+                    _mm_set1_epi8(flipped as i8), // 用于将一个 8 位整数值复制到一个 __m128i 向量的每个元素中。
+                    _mm_loadu_si128(&self.keys as *const [u8; 16] as *const __m128i), // 用于从内存中加载 128 位的整数向量到一个 __m128i 变量中。它可以处理未对齐的内存地址。
+                );
+                // _mm_movemask_epi8，用于将 __m128i 向量中的元素转换为位掩码。它返回一个 16 位整数，其中每个位表示对应元素的高位是否为 1。
+                let bit_field = _mm_movemask_epi8(cmp) & (0xFFFF >> (16 - self.base.meta.count)); // 将超过 self.base.meta.count 位的高位清零，以确保只考虑有效元素。
+                let pos = if bit_field > 0 {
+                    Self::ctz(bit_field as u16)
+                } else {
+                    self.base.meta.count
+                };
+                pos as usize
+            }
+        }
+
+        #[cfg(any(not(target_feature = "sse2"), miri))]
+        {
+            let mut pos = 0;
+            while pos < self.base.meta.count {
+                if self.keys[pos as usize] >= flipped {
+                    return pos as usize;
+                }
+                pos += 1;
+            }
+            pos as usize
+        }
+    }
+
+    fn get_child_pos(&self, key: u8) -> Option<usize> {
+        #[cfg(all(target_feature = "sse2", not(miri)))]
+        unsafe {
+            self.get_child_pos_sse2(key)
+        }
+
+        #[cfg(any(not(target_feature = "sse2"), miri))]
+        self.get_child_pos_linear(key)
+    }
+
+    #[cfg(any(not(target_feature = "sse2"), miri))]
+    fn get_child_pos_linear(&self, key: u8) -> Option<usize> {
+        for i in 0..self.base.meta.count {
+            if self.keys[i as usize] == Self::flip_sign(key) {
+                return Some(i as usize);
+            }
+        }
+        None
+    }
+
+    #[cfg(target_feature = "sse2")]
+    unsafe fn get_child_pos_sse2(&self, key: u8) -> Option<usize> {
+        use std::arch::x86_64::{
+            __m128i, _mm_cmpeq_epi8, _mm_loadu_si128, _mm_movemask_epi8, _mm_set1_epi8,
+        };
+        let cmp = _mm_cmpeq_epi8(
+            _mm_set1_epi8(Self::flip_sign(key) as i8),
+            _mm_loadu_si128(&self.keys as *const [u8; 16] as *const __m128i),
+        );
+        let bit_field = _mm_movemask_epi8(cmp) & ((1 << self.base.meta.count) - 1);
+        if bit_field > 0 {
+            Some(Self::ctz(bit_field as u16) as usize)
+        } else {
+            None
+        }
+    }
+}
+
+impl NodeTrait for Node16 {
+    fn get_type() -> NodeType {
+        NodeType::Node16
+    }
+
+    // fn get_children(&self, start: u8, end: u8) -> NodeIter {
+    //     if self.base.meta.count == 0 {
+    //         // FIXME: the node may be empty due to deletion, this is not intended, we should fix the delete logic
+    //         return NodeIter::N16(Node16Iter {
+    //             node: self,
+    //             start_pos: 1,
+    //             end_pos: 0,
+    //         });
+    //     }
+    //     let start_pos = self.get_child_pos(start).unwrap_or(0);
+    //     let end_pos = self
+    //         .get_child_pos(end)
+    //         .unwrap_or(self.base.meta.count as usize - 1);
+    //
+    //     debug_assert!(end_pos < 16);
+    //
+    //     NodeIter::N16(Node16Iter {
+    //         node: self,
+    //         start_pos,
+    //         end_pos,
+    //     })
+    // }
+
+    fn remove(&mut self, k: u8) {
+        let pos = self
+            .get_child_pos(k)
+            .expect("trying to delete a non-existing key");
+        unsafe {
+            std::ptr::copy(
+                self.keys.as_ptr().add(pos + 1),
+                self.keys.as_mut_ptr().add(pos),
+                self.base.meta.count as usize - pos - 1,
+            );
+
+            std::ptr::copy(
+                self.children.as_ptr().add(pos + 1),
+                self.children.as_mut_ptr().add(pos),
+                self.base.meta.count as usize - pos - 1,
+            );
+        }
+        self.base.meta.count -= 1;
+        debug_assert!(self.get_child(k).is_none());
+    }
+
+    fn copy_to<N: NodeTrait>(&self, dst: &mut N) {
+        for i in 0..self.base.meta.count {
+            dst.insert(
+                Self::flip_sign(self.keys[i as usize]),
+                self.children[i as usize],
+            );
+        }
+    }
+
+    fn base(&self) -> &BaseNode {
+        &self.base
+    }
+
+    fn base_mut(&mut self) -> &mut BaseNode {
+        &mut self.base
+    }
+
+    fn is_full(&self) -> bool {
+        self.base.meta.count == 16
+    }
+
+    // Insert must keep keys sorted, is this necessary?
+    fn insert(&mut self, key: u8, node: NodePtr) {
+        let key_flipped = Self::flip_sign(key);
+
+        let pos = self.get_insert_pos(key);
+
+        unsafe {
+            std::ptr::copy(
+                self.keys.as_ptr().add(pos),
+                self.keys.as_mut_ptr().add(pos + 1),
+                self.base.meta.count as usize - pos,
+            );
+
+            std::ptr::copy(
+                self.children.as_ptr().add(pos),
+                self.children.as_mut_ptr().add(pos + 1),
+                self.base.meta.count as usize - pos,
+            );
+        }
+
+        self.keys[pos] = key_flipped;
+        self.children[pos] = node;
+        self.base.meta.count += 1;
+
+        assert!(self.base.meta.count <= 16);
+    }
+
+    fn change(&mut self, key: u8, val: NodePtr) -> NodePtr {
+        let pos = self.get_child_pos(key).unwrap();
+        let old = self.children[pos];
+        self.children[pos] = val;
+        old
+    }
+
+    fn get_child(&self, key: u8) -> Option<NodePtr> {
+        let pos = self.get_child_pos(key)?;
+        let child = self.children[pos];
+        Some(child)
+    }
+
+
+}
 
 #[repr(C)]
 #[repr(align(8))]
@@ -162,10 +369,198 @@ pub(crate) struct Node48 {
     next_empty: u8,
     children: [NodePtr; 48],
 }
+
+pub(crate) const EMPTY_MARKER: u8 = 48;
+
+impl Node48 {
+    pub(crate) fn init_empty(&mut self) {
+        for v in self.child_idx.iter_mut() {
+            *v = EMPTY_MARKER;
+        }
+        self.next_empty = 0;
+        for (i, child) in self.children.iter_mut().enumerate() {
+            *child = NodePtr::from_tid(i + 1);
+        }
+    }
+}
+impl Node for Node48 {
+    fn get_type() -> NodeType {
+        NodeType::Node48
+    }
+
+    fn remove(&mut self, k: u8) {
+        debug_assert!(self.child_idx[k as usize] != EMPTY_MARKER);
+        let pos = self.child_idx[k as usize];
+        self.children[pos as usize] = NodePtr::from_tid(self.next_empty as usize);
+        self.child_idx[k as usize] = EMPTY_MARKER;
+        self.next_empty = pos;
+        self.base.meta.count -= 1;
+        debug_assert!(self.get_child(k).is_none());
+    }
+
+    // fn get_children(&self, start: u8, end: u8) -> NodeIter {
+    //     NodeIter::N48(Node48Iter {
+    //         start: start as u16,
+    //         end: end as u16,
+    //         node: self,
+    //     })
+    // }
+
+    fn copy_to<N: NodeTrait>(&self, dst: &mut N) {
+        for (i, c) in self.child_idx.iter().enumerate() {
+            if *c != EMPTY_MARKER {
+                dst.insert(i as u8, self.children[*c as usize]);
+            }
+        }
+    }
+
+    fn base(&self) -> &BaseNode {
+        &self.base
+    }
+
+    fn base_mut(&mut self) -> &mut BaseNode {
+        &mut self.base
+    }
+
+    fn is_full(&self) -> bool {
+        self.base.meta.count == 48
+    }
+
+    fn insert(&mut self, key: u8, node: NodePtr) {
+        let pos = self.next_empty as usize;
+        self.next_empty = self.children[pos].as_tid() as u8;
+
+        debug_assert!(pos < 48);
+
+        self.children[pos] = node;
+        self.child_idx[key as usize] = pos as u8;
+        self.base.meta.count += 1;
+    }
+
+    fn change(&mut self, key: u8, val: NodePtr) -> NodePtr {
+        let old = self.children[self.child_idx[key as usize] as usize];
+        self.children[self.child_idx[key as usize] as usize] = val;
+        old
+    }
+
+    fn get_child(&self, key: u8) -> Option<NodePtr> {
+        if self.child_idx[key as usize] == EMPTY_MARKER {
+            None
+        } else {
+            let child = self.children[self.child_idx[key as usize] as usize];
+
+            #[cfg(all(target_feature = "sse2", not(miri)))]
+            {
+                let ptr = child.as_ptr();
+                unsafe {
+                    _mm_prefetch::<_MM_HINT_T0>(ptr as *const i8);
+                }
+            }
+
+            Some(child)
+        }
+    }
+}
+
+
 #[repr(C)]
 #[repr(align(8))]
 pub(crate) struct Node256 {
     base: BaseNode,
     key_mask: [u8; 32],
     children: [NodePtr; 256],
+}
+
+impl Node256 {
+    #[inline]
+    fn set_mask(&mut self, key: usize) {
+        let idx = key / 8;
+        let bit = key % 8;
+        self.key_mask[idx] |= 1 << bit;
+    }
+
+    #[inline]
+    fn unset_mask(&mut self, key: usize) {
+        let idx = key / 8;
+        let bit = key % 8;
+        self.key_mask[idx] &= !(1 << bit);
+    }
+
+    #[inline]
+    fn get_mask(&self, key: usize) -> bool {
+        let idx = key / 8;
+        let bit = key % 8;
+        let key_mask = self.key_mask[idx];
+        key_mask & (1 << bit) != 0
+    }
+}
+
+impl NodeTrait for Node256 {
+    fn base(&self) -> &BaseNode {
+        &self.base
+    }
+
+    // fn get_children(&self, start: u8, end: u8) -> NodeIter {
+    //     NodeIter::N256(Node256Iter {
+    //         start,
+    //         end,
+    //         idx: 0,
+    //         node: self,
+    //     })
+    // }
+
+    fn base_mut(&mut self) -> &mut BaseNode {
+        &mut self.base
+    }
+
+    fn is_full(&self) -> bool {
+        false
+    }
+
+    fn insert(&mut self, key: u8, node: NodePtr) {
+        self.children[key as usize] = node;
+        self.set_mask(key as usize);
+        self.base.meta.count += 1;
+    }
+
+    fn change(&mut self, key: u8, val: NodePtr) -> NodePtr {
+        let old = self.children[key as usize];
+        self.children[key as usize] = val;
+        old
+    }
+
+    fn get_child(&self, key: u8) -> Option<NodePtr> {
+        if self.get_mask(key as usize) {
+            let child = self.children[key as usize];
+
+            #[cfg(all(target_feature = "sse2", not(miri)))]
+            {
+                let ptr = child.as_ptr();
+                unsafe {
+                    _mm_prefetch::<_MM_HINT_T0>(ptr as *const i8);
+                }
+            }
+
+            Some(child)
+        } else {
+            None
+        }
+    }
+
+    fn remove(&mut self, k: u8) {
+        self.unset_mask(k as usize);
+        self.base.meta.count -= 1;
+    }
+
+    fn copy_to<N: Node>(&self, dst: &mut N) {
+        for (i, c) in self.children.iter().enumerate() {
+            if self.get_mask(i) {
+                dst.insert(i as u8, *c);
+            }
+        }
+    }
+
+    fn get_type() -> NodeType {
+        NodeType::Node256
+    }
 }
